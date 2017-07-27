3448bf27027bc380367f9c8b518e8d7f380f155f
akif121 <akif.hamid33@gmail.com>
2017-07-28 04:15:35 +0530

    angularLearning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,8 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <ul>
-      <li>
-        <a routerLink="/">Home</a>
-      </li>
-      <li>
-        <a routerLink="/About">About</a>
-      </li>
+      <li> <a routerLink="/">Home</a></li>
+      <li> <a routerLink="/About">About</a> </li>
     </ul>
 
   <router-outlet></router-outlet>
